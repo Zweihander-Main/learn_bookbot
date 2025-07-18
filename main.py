297031907663a04121064baa25402d22a321d444
Zweihänder <zweidev@zweihander.me>
2025-07-17 21:33:20 -0400

    feat(main): allow for arg book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from custom_types import CharDataSortedList
 from stats import get_char_data, get_num_words, get_sorted_char_data
 
@@ -18,8 +19,14 @@ def main() -> None:
 	"""
 	Main function to execute the book reading functionality.
 	"""
-	relative_path = 'books/frankenstein.txt'
 	book_text = ""
+
+
+	if len(sys.argv) < 2:
+		print("Usage: python3 main.py <path_to_book>")
+		print("Example: python3 main.py books/book.txt")
+		exit(1)
+	relative_path: str = sys.argv[1]
 
 	print("============ BOOKBOT ============")
 	print(f"Analyzing book found at {relative_path}...")
@@ -28,14 +35,14 @@ def main() -> None:
 		book_text = get_book_text(relative_path)
 	except FileNotFoundError:
 		print(f"Error: The file '{relative_path}' was not found.")
-		return
+		exit(1)
 	except Exception as e:
 		print(f"An error occurred: {e}")
-		return
+		exit(1)
 
 	if not book_text:
 		print("The book is empty or could not be read.")
-		return
+		exit(1)
 
 	print("----------- Word Count ----------")
 	num_words = get_num_words(book_text)
